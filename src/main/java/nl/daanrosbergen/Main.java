99ed1b90f6c58f38322bd870ada26eea3b68ca7e
Daan Rosbergen <d.rosbergen@gmail.com>
2016-06-28 17:32:40 +0200

    Expand main method with linked list

--- src/main/java/nl/daanrosbergen/Main.java
+++ src/main/java/nl/daanrosbergen/Main.java
@@ -1,6 +1,9 @@
 package nl.daanrosbergen;
 
 import nl.daanrosbergen.bubblesort.BubbleSort;
+import nl.daanrosbergen.linkedlist.Iterator;
+import nl.daanrosbergen.linkedlist.Lijst;
+import nl.daanrosbergen.linkedlist.LijstImpl;
 import nl.daanrosbergen.mergesort.MergeSort;
 
 public class Main {
@@ -15,5 +18,31 @@ public class Main {
         System.out.println(merge);
         MergeSort.sort(merge);
         System.out.println(merge);
+
+        Lijst lijst = new LijstImpl();
+
+        lijst.addFirst( "Drie" );
+        lijst.addFirst( "Twee" );
+        lijst.addFirst( "Een" );
+
+        lijst.addLast( "Vier" );
+        lijst.addLast( "Vijf" );
+        lijst.addLast( "Zes" );
+
+        System.out.println("getFirst: " + lijst.getFirst());
+        System.out.println("getLast: " + lijst.getLast());
+        System.out.println(lijst.removeFirst() + " is verwijderd [removeFirst]");
+        System.out.println(lijst.removeLast() + " is verwijderd [removeLast]");
+
+        Iterator iter = lijst.iterator();
+
+        while( iter.hasNext() )
+            System.out.println( iter.next() );
+
+        System.out.println("\n En nu omgekeerd : \n");
+
+        iter = lijst.iterator();
+        while( iter.hasPrevious() )
+            System.out.println( iter.previous() );
     }
 }
